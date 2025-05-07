e6b477183ef0f96579b056b0b9f4b6ab8867f533
Lourrayni Feliph <lourraynidantass@gmail.com>
2025-05-07 15:03:30 -0300

    feat:adicionando services

---
+++ estudos_php/Colecao/app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+ use App\Services\UserService;
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Auth;
+ 
+ class UserController extends Controller
+ {
+     private $userService;
+ 
+     public function __construct(UserService $userService){
+         $this->userService = $userService;
+     }
+ 
+     public function formLogin()
+     {
+         return view('formLogin');
+     }
+ 
+     public function login(Request $request)
+     {
+         if ($this->userService->login(['email' => $request->email, 'password' => $request->password])) {
+             return redirect()->route('restrita');
+         }
+         return redirect()->back()->with(['mensagem' => 'E-mail ou senha inválidos!']);
+     }
+ 
+     public function logout()
+     {
+         $this->userService->logout();
+         return redirect()->route('formLogin');
+     }
+ 
+     public function restrita()
+     {
+         return view('restrita');
+     }
+ }
--- estudos_php/Colecao/routes/web.php
+++ estudos_php/Colecao/routes/web.php
@@ -1,31 +1,14 @@
 <?php
 
-use App\Models\User;
-use App\Models\Figura;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TesteController;
+use PHPUnit\Event\Code\Test;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/users', ['uses' => 'TesteController@users']);
+Route::get('/', [UserController::class, 'formLogin'])->name('formLogin');
+Route::post('/', [UserController::class, 'login'])->name('login');
 
-Route::get('/sortearNumero', function(){
-    print(rand(1, 10));
-});
-
-Route::get('/pesquisarFigura/{nome?}', function(string $nome = 'sem nome'){
-    $figuras = Figura::where('nome', 'LIKE', '%' . $nome . '%')->get();
-    foreach($figuras as $figura){
-        print("<b>Nome:</b> {$figura->nome} - <b>Figura:</b> {$figura->categoria->nome} - <b>Prateleira:</b> {$figura->prateleira->nome} - <b>Coleção:</b> {$figura->categoria->colecao->nome}");
-        print('<br>');
-    }
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [UserController::class, 'logout'])->name('logoff');
+    Route::get('/restrita', [UserController::class, 'restrita'])->name('restrita');
 });
